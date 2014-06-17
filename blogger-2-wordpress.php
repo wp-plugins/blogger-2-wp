e494c35a0deb8a21693fb877618063f537c2523a
subins2000 <subins2000@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-17 06:49:06 +0000

    tagging version 0.5
    git-svn-id: https://plugins.svn.wordpress.org/blogger-2-wp/tags/0.5@933481 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- blogger-2-wordpress.php
+++ blogger-2-wordpress.php
@@ -3,34 +3,33 @@
  * Plugin Name: Blogger 2 WordPress
  * Plugin URI: http://subinsb.com/move-blog-from-blogger-to-wordpress
  * Description: Move your Blogger Blog to Wordpress with Posts Redirection. Example : myblog.blogspot.com/2014/02/post-1 to wordpressblog.com/post-1. You can also redirect Blogger Pages to your New Wordpress Pages. Easily move from old abc.blogspot.com to new domain abc.com
- * Version: 0.3
+ * Version: 0.4.5
  * Author: Subin Siby
  * Author URI: http://subinsb.com
  * License: GPLv3
 */
 function b2w_admin_menu() {
  add_submenu_page('plugins.php', __('Blogger 2 WordPress'), __('Blogger 2 WordPress'), 'manage_options', 'b2w_admin', 'bToW_options_page');
- 
- if (isset($_GET['page']) && $_GET['page'] == 'blogger-2-wordpress-admin') {
-  /*wp_enqueue_script('dashboard');
-  wp_enqueue_style('dashboard');*/
- }
+ /*if (isset($_GET['page']) && $_GET['page'] == 'blogger-2-wordpress-admin') {
+  wp_enqueue_script('dashboard');
+  wp_enqueue_style('dashboard');
+ }*/
 }
 add_action('admin_menu', 'b2w_admin_menu');
 function bloggerTowordpressAdminPage(){
- echo'<div class="wrap">';
- if(isset($_POST['pages'])){
-  $pages=str_replace('\"', '"', $_POST['pages']);
-  if($pages!=""){
-   if(is_object(json_decode($pages))){
-    update_option("b2wps048", $pages);
-    echo '<div id="message" class="updated"><p>Added Pages For Redirection</p></div>';
-   }else{
-    echo '<div id="message" class="error"><p>Not A Valid Data</p></div>';
+ if(isset($_POST['pfrom']) && isset($_POST['pto'])){
+  $pgsj=array();
+  foreach($_POST['pfrom'] as $k=>$fromVal){
+   $toVal=$_POST['pto'][$k];
+   if($toVal!="" && $fromVal!=""){
+    $pgsj[$toVal]=$fromVal;
    }
   }
+  $pages=json_encode($pgsj);
+  update_option("b2wps048", $pages);
+  echo'<div class="wrap"><div id="message" style="height: 50px;" class="updated"><p style="font-size: 19px;">Added Pages For Redirection</p></div></div>';
  }
-  $wpBlogURL=get_site_url();
+ $wpBlogURL=get_site_url();
 ?>
  <div id="poststuff">
   <div id="post-body" class="metabox-holder columns-2">
@@ -39,7 +38,7 @@ function bloggerTowordpressAdminPage(){
      <div class="inside">
       <h2>Donate</h2>
       <p>I'm 14 and I need donations to create more plugins.</p>
-      <p>Please consider a donation for the improvement of this plugin and for future awesome plugins. (Default <strong>5$</strong>)</p>
+      <p>Please consider a donation for the improvement of this plugin and for future awesome plugins.</p>
        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
         <input type="hidden" name="cmd" value="_s-xclick">
         <input type="hidden" name="hosted_button_id" value="ZYQWUZ2B8ZXXA">
@@ -75,26 +74,57 @@ function bloggerTowordpressAdminPage(){
       </div>
      </div>
      <div style="font-size:16px;margin-top:15px;line-height:2em;" class="postbox">
-      <div class="inside">
+      <div class="inside b2wOpt">
        <h3 class="hndle"><span>Pages Redirection</span></h3><br/>
        <form method="post" action="plugins.php?page=b2w_admin">
-        If your Blogger blog have pages that you want to redirect, add them as JSON array (Example Below) :<br/>
-        <textarea cols="60" rows="10" name="pages"><?echo get_option("b2wps048");?></textarea><br/>
-        <p>DO NOT USE FULL URL. Since Blogger Blogs have multiple domains (com, in, au), <br/>URL of Pages will be different. So only add the URL Path Name.</p>
-        <input type="submit" value="Add pages redirection"/>
+        <p>If your Blogger blog have pages that you want to redirect, add them (Example below this) :</p>
+        <div style="margin:10px;">
+         <?
+         $pages=get_option("b2wps048");
+         $pages=json_decode($pages, true);
+         if(is_array($pages) && count($pages)!=0){
+          foreach($pages as $k=>$v){
+           echo "<div class='b2wOptPR'>";
+            echo "<input type='text' name='pfrom[]' placeholder='From' value='$v'/>";
+            echo "<input type='text' name='pto[]' placeholder='To' value='$k'/>";
+            echo "<a href='javascript:void(0);' onclick='jQuery(this).parent().remove();'>Remove</a>";
+           echo "</div>";
+          }
+         }else{
+          echo "<div class='b2wOptPR'>";
+           echo "<input type='text' name='pfrom[]' placeholder='From' value=''/>";
+           echo "<input type='text' name='pto[]' placeholder='To' value=''/>";
+          echo "</div>";
+         }
+         ?>
+         <a href="javascript:void(0);" style="margin:5px;display:block;" onclick="jQuery('.b2wOptPR:last').after('<div id=\'b2wOptPR\'><input type=\'text\' name=\'pfrom[]\' placeholder=\'From\' value=\'\'/><input type=\'text\' name=\'pto[]\' placeholder=\'To\' value=\'\'/><a href=\'javascript:void(0);\' onclick=\'jQuery(this).parent().remove();\'>Remove</a></div>');">Add New Field</a>
+        </div>
+        <p>DO NOT USE FULL URL. Since Blogger Blogs have multiple domains (com, in, au), URL of Pages will be different. So only add the URL Path.</p>
+        <input type="submit" value="Update Pages Redirection"/>
        </form>
        <h4>Example</h4>
-       <textarea cols="60" rows="10" disabled="disabled">{
-"<?echo$wpBlogURL;?>/page" : "/p/page.html", 
-"<?echo$wpBlogURL;?>/page2" : "/p/page2.html",
-"<?echo$wpBlogURL;?>/contact" : "/p/contact.html"
-}</textarea>
+       <?
+       $pages=array(
+        "page" => "/p/page.html",
+        "page2" => "/p/page2.html",
+        "contact" => "/p/contact.html"
+       );
+       foreach($pages as $k=>$v){
+        echo "<div>";
+         echo "<input type='text' disabled='disabled' placeholder='From' value='$v'/>";
+         $toURL=substr($wpBlogURL, 0, -1)=="/" ? $k:"/$k";
+         echo "<input type='text' disabled='disabled' placeholder='To' value='$wpBlogURL$toURL'/>";
+        echo "</div>";
+       }
+       ?>
+       <style>
+       .b2wOpt input[type=text]{min-width:300px;}
+       </style>
       </div>
      </div>
     </div>
    </div>
   </div>
- </div>
 <?
 }
 function bToW_options_page(){
@@ -145,12 +175,24 @@ function bToW_options_page(){
 function StartB2WRedirection() {
  global $_SERVER;
  global $wp_query;
+ global $wp;
+ global $wpdb;
  $b2w = (isset($_GET['b2wURL'])) ? $_GET['b2wURL']:false;
+ $current_url = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
+ $current_url = parse_url($current_url);
+ $current_url = $current_url['host'].$current_url['path'];
+ $siteURL=get_home_url();
+ $siteURLParts=parse_url($siteURL);
+ $siteURL=$siteURLParts['host'].$siteURLParts['path'];
  if(!$b2w && $wp_query->is_404===true){
-  $b2w=$_SERVER['REQUEST_URI'];
+  $b2w=str_replace($siteURL, "", $current_url);
  }
  if($b2w){
-  global $wpdb;
+  $pgs=get_option("b2wps048");
+  $pgs=json_decode($pgs, true);
+  if(is_array($pgs)){
+   $gtun=array_search($b2w, $pgs);
+  }
   $b2w=str_replace(".html", "", $b2w);
   $sql = "SELECT DISTINCT meta_value FROM {$wpdb->postmeta} where meta_key = 'blogger_blog'";
   $results = $wpdb->get_results($sql);
@@ -163,10 +205,6 @@ function StartB2WRedirection() {
      $b2w = strstr($b2w,'?',true);
     }
    }
-   $pgs=json_decode(get_option("b2wps048"), true);
-   if(is_array($pgs)){
-    $gtun=array_search($b2w,$pgs);
-   }
    $sqlstr = $wpdb->prepare("SELECT wposts.ID, wposts.guid
     FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
     WHERE wposts.ID = wpostmeta.post_id
@@ -174,14 +212,14 @@ function StartB2WRedirection() {
     AND wpostmeta.meta_value LIKE %s", "%$b2w%"
    );
    $wpurl = $wpdb->get_results($sqlstr, ARRAY_N);
-   if($wpurl){
-    $prvt=get_permalink($wpurl[0][0]);
-    header( 'Location: '.$prvt.' ',true,301) ;
-    exit;
-   }elseif($gtun!=false){
-    header('Location: '.$gtun.' ',true,301);
-    exit;
-   }
+  }
+  if(isset($wpurl) && $wpurl){
+   $prvt=get_permalink($wpurl[0][0]);
+   header( 'Location: '.$prvt.' ',true,301) ;
+   exit;
+  }elseif($gtun!=false){
+   header('Location: '.$gtun.' ',true,301);
+   exit;
   }
  }
 }
--- readme.txt
+++ readme.txt
@@ -3,10 +3,11 @@ Contributors: Subin Siby
 Donate link: http://subinsb.com
 Tags: wordpress, migrate, blogger, blog, redirect, import, posts, pages, redirection
 Requires at least: 3.0.1
-Tested up to: 3.8.1
-Stable tag: 0.3
+Tested up to: 3.9
+Stable tag: 0.4.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: blogger-2-wp
 
 Set up redirection of posts, pages from Blogger Blog to Wordpress.
 
@@ -15,13 +16,15 @@ Set up redirection of posts, pages from Blogger Blog to Wordpress.
 Set up redirections from your old blogger blog to your new WordPress blog. You can redirect posts, pages and as well as the home page. This plugin will generate a template for your blogger blog that will redirect your old Blogger users to your new WordPress blog.
 This plugin will get the URL the blogger user visited and redirect to the new post. See ( http://subinsb.com/move-blog-from-blogger-to-wordpress ) for more details.
 
+Languages: Also available in Spanish & Serbian languages
+
 **Compatibilty**
 
 Blogger 2 WP works on Wordpress versions from 3.0.1 to the latest WordPress version without any problem.
 
 **Donate**
 
-I'm only 14 years old when writing this plugin. Please consider a donation to support me. Please....
+I was 14 years old when writing this plugin. Please consider a donation to support me. Please....
 http://subinsb.com/donate
 
 **Support**
@@ -51,15 +54,36 @@ You can find [screenshots here](http://subinsb.com/move-blog-from-blogger-to-wor
 = NOTICE =
 All users who updated this plugin to Version 0.2 should reinstall Template Code.
 
+= 0.4.5 =
+1) Fixed posts redirection bug
+
+2) Replaced Pages Redirection JSON format in settings to Input Form
+
+3) Fixed Options Page Bugs
+
+= 0.4 =
+1) Fixed Bugs
+
+2) Fixed wrong display of settings success/error messages
+
+3) Improved Pages redirection
+
+4) Works on Wordpress installations in /dir/disdf directories too
+
 = 0.3 =
 1) Checks If 404 Pages matches a blogger post you imported
+
 2) Fixed Redirection WWW bug
+
 3) Redesigned Options Page
+
 4) Added Donate Option (Please Donate...)
 
 = 0.2 =
 1) Fixed Pages Redirection bug.
+
 2) Improved Easabilty To Use
+
 3) New Template Code
 
 = 0.1 =
